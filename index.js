07df64083e8752f193d78ccc744182a666b80ad5
Austin Frick <austin.frick429@gmail.com>
2023-09-13 12:04:17 -0400

    modified index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+const divide = (parameter1, parameter2) => {
+ console.log (`dividing ${2000}`) 
+ console.log (`dividing ${100}`) 
+ return parameter1 / parameter2 
+}
+
+const add = (parameter1, parameter2) => {
+    console.log (`adding ${3}`) 
+    console.log (`adding ${4}`) 
+    return parameter1 + parameter2
+} 
+
+const num = [2] 
+ square = x => x * x;
+console.log (`square ${2}`) 
+
